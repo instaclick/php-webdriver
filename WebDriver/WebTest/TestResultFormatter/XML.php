6f9930a3653c2589e3c74d37d52b01776ac1c973
Anthon Pang <apang@softwaredevelopment.ca>
2011-11-23 22:41:23 -0500

    remove WebTest folder

--- WebDriver/WebTest/TestResultFormatter/XML.php
+++
@@ -1,2 +0,0 @@
-// @see http://svn.apache.org/repos/asf/ant/core/trunk/src/main/org/apache/tools/ant/taskdefs/optional/junit/XMLJUnitResultFormatter.java
-// @see http://www.phpunit.de/manual/current/en/logging.html
