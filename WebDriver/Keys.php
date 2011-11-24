ad6a6d72123f25407a9b2904472bac165943ba06
Anthon Pang <apang@softwaredevelopment.ca>
2011-11-23 23:26:49 -0500

    use consts and pre-convert

--- WebDriver/Keys.php
+++ WebDriver/Keys.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Copyright 2004-present Facebook. All Rights Reserved.
+ * Copyright 2011 Anthon Pang. All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -23,81 +23,67 @@
  * @package WebDriver
  */
 final class WebDriver_Keys {
-  /*
-   * The Unicode "Private Use Area" code points (0xE000-0xF8FF) are used to represent
-   * pressable, non-text keys.
-   */
-  static private $key_strokes = array(
-    'NULL' => "\xE0\x00",
-    'Cancel' => "\xE0\x01",
-    'Help' => "\xE0\x02",
-    'Back space' => "\xE0\x03",
-    'Tab' => "\xE0\x04",
-    'Clear' => "\xE0\x05",
-    'Return1' => "\xE0\x06",
-    'Enter1' => "\xE0\x07",
-    'Shift' => "\xE0\x08",
-    'Control' => "\xE0\x09",
-    'Alt' => "\xE0\x0A",
-    'Pause' => "\xE0\x0B",
-    'Escape' => "\xE0\x0C",
-    'Space' => "\xE0\x0D",
-    'Pageup' => "\xE0\x0E",
-    'Pagedown' => "\xE0\x0F",
-    'End' => "\xE0\x10",
-    'Home' => "\xE0\x11",
-    'Left arrow' => "\xE0\x12",
-    'Up arrow' => "\xE0\x13",
-    'Right arrow' => "\xE0\x14",
-    'Down arrow' => "\xE0\x15",
-    'Insert' => "\xE0\x16",
-    'Delete' => "\xE0\x17",
-    'Semicolon' => "\xE0\x18",
-    'Equals' => "\xE0\x19",
-    'Numpad 0' => "\xE0\x1A",
-    'Numpad 1' => "\xE0\x1B",
-    'Numpad 2' => "\xE0\x1C",
-    'Numpad 3' => "\xE0\x1D",
-    'Numpad 4' => "\xE0\x1E",
-    'Numpad 5' => "\xE0\x1F",
-    'Numpad 6' => "\xE0\x20",
-    'Numpad 7' => "\xE0\x21",
-    'Numpad 8' => "\xE0\x22",
-    'Numpad 9' => "\xE0\x23",
-    'Multiply' => "\xE0\x24",
-    'Add' => "\xE0\x25",
-    'Separator' => "\xE0\x26",
-    'Subtract' => "\xE0\x27",
-    'Decimal' => "\xE0\x28",
-    'Divide' => "\xE0\x29",
-    'F1' => "\xE0\x31",
-    'F2' => "\xE0\x32",
-    'F3' => "\xE0\x33",
-    'F4' => "\xE0\x34",
-    'F5' => "\xE0\x35",
-    'F6' => "\xE0\x36",
-    'F7' => "\xE0\x37",
-    'F8' => "\xE0\x38",
-    'F9' => "\xE0\x39",
-    'F10' => "\xE0\x3A",
-    'F11' => "\xE0\x3B",
-    'F12' => "\xE0\x3C",
-    'Command/Meta' => "\xE0\x3D",
-  );
-
-  /**
-   * Get key stroke code for pressable, non-text keys
-   *
-   * @link http://code.google.com/p/selenium/wiki/JsonWireProtocol#/session/:sessionId/element/:id/value
-   *
-   * @param string $key
-   * @return string
-   */
-  static function key($key) {
-    if (!isset(self::$key_strokes[$key])) {
-      return '';
-    }
-
-    return iconv('UCS-2', 'UTF-8', self::$key_strokes[$key]);
-  }
+	/*
+	 * The Unicode "Private Use Area" code points (0xE000-0xF8FF) are used to represent
+	 * pressable, non-text keys.
+	 *
+	 * @link http://code.google.com/p/selenium/wiki/JsonWireProtocol#/session/:sessionId/element/:id/value
+	 *    key_name   = "UTF-8";        // UCS-2
+	 */
+	const NullKey    = "\xEE\x80\x80"; // E000
+	const Cancel     = "\xEE\x80\x81"; // E001
+	const Help       = "\xEE\x80\x82"; // E002
+	const BackSpace  = "\xEE\x80\x83"; // E003
+	const Tab        = "\xEE\x80\x84"; // E004
+	const Clear      = "\xEE\x80\x85"; // E005
+	const ReturnKey  = "\xEE\x80\x86"; // E006
+	const Enter      = "\xEE\x80\x87"; // E007
+	const Shift      = "\xEE\x80\x88"; // E008
+	const Control    = "\xEE\x80\x89"; // E009
+	const Alt        = "\xEE\x80\x8A"; // E00A
+	const Pause      = "\xEE\x80\x8B"; // E00B
+	const Escape     = "\xEE\x80\x8C"; // E00C
+	const Space      = "\xEE\x80\x8D"; // E00D
+	const PageUp     = "\xEE\x80\x8E"; // E00E
+	const PageDown   = "\xEE\x80\x8F"; // E00F
+	const End        = "\xEE\x80\x90"; // E010
+	const Home       = "\xEE\x80\x91"; // E011
+	const LeftArrow  = "\xEE\x80\x92"; // E012
+	const UpArrow    = "\xEE\x80\x93"; // E013
+	const RightArrow = "\xEE\x80\x94"; // E014
+	const DownArrow  = "\xEE\x80\x95"; // E015
+	const Insert     = "\xEE\x80\x96"; // E016
+	const Delete     = "\xEE\x80\x97"; // E017
+	const Semicolon  = "\xEE\x80\x98"; // E018
+	const Equals     = "\xEE\x80\x99"; // E019
+	const Numpad0    = "\xEE\x80\x9A"; // E01A
+	const Numpad1    = "\xEE\x80\x9B"; // E01B
+	const Numpad2    = "\xEE\x80\x9C"; // E01C
+	const Numpad3    = "\xEE\x80\x9D"; // E01D
+	const Numpad4    = "\xEE\x80\x9E"; // E01E
+	const Numpad5    = "\xEE\x80\x9F"; // E01F
+	const Numpad6    = "\xEE\x80\xA0"; // E020
+	const Numpad7    = "\xEE\x80\xA1"; // E021
+	const Numpad8    = "\xEE\x80\xA2"; // E022
+	const Numpad9    = "\xEE\x80\xA3"; // E023
+	const Multiply   = "\xEE\x80\xA4"; // E024
+	const Add        = "\xEE\x80\xA5"; // E025
+	const Separator  = "\xEE\x80\xA6"; // E026
+	const Subtract   = "\xEE\x80\xA7"; // E027
+	const Decimal    = "\xEE\x80\xA8"; // E028
+	const Divide     = "\xEE\x80\xA9"; // E029
+	const F1         = "\xEE\x80\xB1"; // E031
+	const F2         = "\xEE\x80\xB2"; // E032
+	const F3         = "\xEE\x80\xB3"; // E033
+	const F4         = "\xEE\x80\xB4"; // E034
+	const F5         = "\xEE\x80\xB5"; // E035
+	const F6         = "\xEE\x80\xB6"; // E036
+	const F7         = "\xEE\x80\xB7"; // E037
+	const F8         = "\xEE\x80\xB8"; // E038
+	const F9         = "\xEE\x80\xB9"; // E039
+	const F10        = "\xEE\x80\xBA"; // E03A
+	const F11        = "\xEE\x80\xBB"; // E03B
+	const F12        = "\xEE\x80\xBC"; // E03C
+	const Command    = "\xEE\x80\xBD"; // E03D
+	const Meta       = "\xEE\x80\xBD"; // E03D
 }
