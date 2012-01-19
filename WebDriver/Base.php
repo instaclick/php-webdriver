5c4fc30480a458d8725ba3464ef0c627ab2746e2
Anthon Pang <apang@softwaredevelopment.ca>
2012-01-19 00:48:49 -0500

    apply tszming patch, and fix original header too

--- WebDriver/Base.php
+++ WebDriver/Base.php
@@ -97,7 +97,7 @@ abstract class WebDriver_Base {
 
 		$curl = WebDriver_Environment::CurlInit($http_method, $url, $params);
 		curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-		curl_setopt($curl, CURLOPT_HTTPHEADER, array('application/json;charset=UTF-8'));
+		curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-Type: application/json;charset=UTF-8', 'Accept: application/json'));
 
 		if ($http_method === 'POST') {
 			curl_setopt($curl, CURLOPT_POST, true);
