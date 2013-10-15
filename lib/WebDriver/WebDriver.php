786ba0dc9c594fb0b9b8171db852b2224b23811e
Anthon Pang <anthonp@nationalfibre.net>
2013-10-15 10:51:06 -0400

    fix issue #38 - fix capabilities handling

--- lib/WebDriver/WebDriver.php
+++ lib/WebDriver/WebDriver.php
@@ -45,33 +45,34 @@ final class WebDriver extends AbstractWebDriver
      * New Session: /session (POST)
      * Get session object for chaining
      *
-     * @param mixed $requiredCapabilities Required capabilities (or browser name)
-     * @param array $desiredCapabilities  Desired capabilities
+     * @param array|string $requiredCapabilities Required capabilities (or browser name)
+     * @param array        $desiredCapabilities  Desired capabilities
      *
      * @return \WebDriver\Session
      */
     public function session($requiredCapabilities = Browser::FIREFOX, $desiredCapabilities = array())
     {
-        if (!is_array($desiredCapabilities)) {
-            $desiredCapabilities = array();
-        }
-
-        if (!is_array($requiredCapabilities)) {
-            $desiredCapabilities = array_merge(
-                $desiredCapabilities,
-                array(Capability::BROWSER_NAME => $requiredCapabilities)
-            );
+        // for backwards compatibility when the only required capability was browser name
+        if (! is_array($requiredCapabilities)) {
+            $desiredCapabilities[Capability::BROWSER_NAME] = $requiredCapabilities ?: Browser::FIREFOX;
 
             $requiredCapabilities = array();
+        }
+
+        // required
+        $parameters = array(
+            'desiredCapabilities' => array_merge($desiredCapabilities, $requiredCapabilities)
+        );
+
+        // optional
+        if ( ! empty($requiredCapabilities)) {
+            $parameters['requiredCapabilities'] = $requiredCapabilities;
         }
 
         $results = $this->curl(
             'POST',
             '/session',
-            array(
-                'desiredCapabilities' => $desiredCapabilities,
-                'requiredCapabilities' => $requiredCapabilities,
-            ),
+            $parameters,
             array(CURLOPT_FOLLOWLOCATION => true)
         );
 
