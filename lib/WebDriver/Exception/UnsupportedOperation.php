7862f3e6ed7564ffb8c0f53d550b4fc594d57086
Anthon Pang <apang@softwaredevelopment.ca>
2020-02-21 16:12:42 -0500

    Update copyright year

--- lib/WebDriver/Exception/UnsupportedOperation.php
+++ lib/WebDriver/Exception/UnsupportedOperation.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Copyright 2019-2019 Facebook. All Rights Reserved.
+ * Copyright 2019-2020 Facebook. All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
