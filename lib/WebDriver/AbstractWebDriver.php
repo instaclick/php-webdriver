0d301b22ed76bcccefdab51d29aa8e8fcb8c91aa
Michael Voříšek <mvorisek@mvorisek.cz>
2022-04-18 01:41:08 +0200

    Assert curl data are always fully serialized before request

--- lib/WebDriver/AbstractWebDriver.php
+++ lib/WebDriver/AbstractWebDriver.php
@@ -161,6 +161,8 @@ abstract class AbstractWebDriver
             $url .= '/' . $parameters;
         }
 
+        $this->assertNonObjectParameters($parameters);
+
         list($rawResult, $info) = $this->curlService->execute(
             $requestMethod,
             $url,
@@ -242,6 +244,32 @@ abstract class AbstractWebDriver
             'info'       => $info,
             'sessionId'  => $sessionId,
             'sessionUrl' => $sessionId ? $this->url . '/session/' . $sessionId : $info['url'],
+        );
+    }
+
+    /**
+     * @param mixed $parameters
+     */
+    private function assertNonObjectParameters($parameters)
+    {
+        if ($parameters === null || is_scalar($parameters)) {
+            return;
+        }
+
+        if (is_array($parameters)) {
+            foreach ($parameters as $value) {
+                $this->assertNonObjectParameters($value);
+            }
+
+            return;
+        }
+
+        throw WebDriverException::factory(
+            WebDriverException::UNEXPECTED_PARAMETERS,
+            sprintf(
+                "Unable to serialize non-scalar type %s",
+                is_object($parameters) ? get_class($parameters) : gettype($parameters)
+            )
         );
     }
 
