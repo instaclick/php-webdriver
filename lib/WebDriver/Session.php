c5e230cf8bb4ab1307fc59009b987559aa5562e9
Phil Davis <phil@jankaritech.com>
2023-12-08 12:45:15 +0545

    fix: catch ScriptTimeout exception into a variable

--- lib/WebDriver/Session.php
+++ lib/WebDriver/Session.php
@@ -48,7 +48,6 @@ use WebDriver\Exception as WebDriverException;
  * @method void postAlert_text($jsonText) Sends keystrokes to a JavaScript prompt() dialog.
  * @method void accept_alert() Accepts the currently displayed alert dialog.
  * @method void dismiss_alert() Dismisses the currently displayed alert dialog.
- * @method void moveto($jsonCoordinates) Move the mouse by an offset of the specified element (or current mouse cursor).
  * @method void click($jsonButton) Click any mouse button (at the coordinates set by the last moveto command).
  * @method void buttondown() Click and hold the left mouse button (at the coordinates set by the last moveto command).
  * @method void buttonup() Releases the mouse button previously held (where the mouse is currently at).
@@ -305,7 +304,7 @@ final class Session extends Container
     {
         try {
             $result = $this->curl('POST', '/moveto', $parameters);
-        } catch (WebDriverException\ScriptTimeout) {
+        } catch (WebDriverException\ScriptTimeout $e) {
             throw WebDriverException::factory(WebDriverException::UNKNOWN_ERROR);
         }
 
