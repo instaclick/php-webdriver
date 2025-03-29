d6d01b41f038dfc7f44540bc45d6d8a2d7cdb1ee
Anthon Pang <apang@softwaredevelopment.ca>
2025-03-29 02:22:39 -0400

    refs #134 - add filtering and partial re-mapping of capabilities

--- lib/WebDriver/WebDriver.php
+++ lib/WebDriver/WebDriver.php
@@ -4,8 +4,6 @@
  * @copyright 2004 Meta Platforms, Inc.
  * @license Apache-2.0
  *
- * @package WebDriver
- *
  * @author Justin Bishop <jubishop@gmail.com>
  */
 
@@ -14,9 +12,10 @@ namespace WebDriver;
 /**
  * WebDriver class
  *
- * @package WebDriver
- *
+ * W3C
  * @method array status() Returns information about whether a remote end is in a state in which it can create new sessions.
+ * Selenium
+ * @method array logs() Returns session logs mapped to session IDs.
  */
 class WebDriver extends AbstractWebDriver implements WebDriverInterface
 {
@@ -26,13 +25,61 @@ class WebDriver extends AbstractWebDriver implements WebDriverInterface
     private $capabilities;
 
     /**
+     * @var array
+     */
+    static $w3cCapabilities = [
+        Capability::ACCEPT_INSECURE_CERTS => 1,
+        Capability::BROWSER_NAME => 1,
+        Capability::BROWSER_VERSION => 1,
+        Capability::PAGE_LOAD_STRATEGY => 1,
+        Capability::PLATFORM_NAME => 1,
+        Capability::PROXY => 1,
+        Capability::SET_WINDOW_RECT => 1,
+        Capability::STRICT_FILE_INTERACTABILITY => 1,
+        Capability::TIMEOUTS => 1,
+        Capability::UNHANDLED_PROMPT_BEHAVIOR => 1,
+        Capability::USER_AGENT => 1,
+    ];
+
+    /**
+     * @var array
+     */
+    static $w3cToLegacy = [
+        Capability::PLATFORM_NAME         => Capability::PLATFORM,
+        Capability::BROWSER_VERSION       => Capability::VERSION,
+        Capability::ACCEPT_INSECURE_CERTS => Capability::ACCEPT_SSL_CERTS,
+    ];
+
+    /**
+     * @var array|null
+     */
+    static $legacyToW3c = [
+        Capability::PLATFORM         => Capability::PLATFORM_NAME,
+        Capability::VERSION          => Capability::BROWSER_VERSION,
+        Capability::ACCEPT_SSL_CERTS => Capability::ACCEPT_INSECURE_CERTS,
+    ];
+
+    /**
      * {@inheritdoc}
      */
     protected function methods()
     {
-        return array(
-            'status' => 'GET',
-        );
+        return [
+            'status' => ['GET'],
+
+            // Selenium
+            'logs'   => ['POST'],
+        ];
+    }
+
+    /**
+     * Constructor
+     *
+     * @param string $url
+     */
+    public function __construct($url)
+    {
+        parent::__construct($url);
     }
 
     /**
@@ -41,17 +88,18 @@ class WebDriver extends AbstractWebDriver implements WebDriverInterface
     public function session($browserName = Browser::FIREFOX, $desiredCapabilities = null, $requiredCapabilities = null)
     {
         // default to W3C WebDriver API
-        $firstMatch = $desiredCapabilities ? array($desiredCapabilities) : array();
-        $firstMatch[] = array('browserName' => Browser::CHROME);
+        $capabilities = is_array($desiredCapabilities) ? $this->filter($this->remap(self::$legacyToW3c, $desiredCapabilities)) : null;
+        $firstMatch = $capabilities ? [$capabilities] : [];
+        $firstMatch[] = [Capability::BROWSER_NAME => Browser::CHROME];
 
         if ($browserName !== Browser::CHROME) {
-            $firstMatch[] = array('browserName' => $browserName);
+            $firstMatch[] = [Capability::BROWSER_NAME => $browserName];
         }
 
-        $parameters = array('capabilities' => array('firstMatch' => $firstMatch));
+        $parameters = ['capabilities' => ['firstMatch' => $firstMatch]];
 
         if (is_array($requiredCapabilities) && count($requiredCapabilities)) {
-            $parameters['capabilities']['alwaysMatch'] = $requiredCapabilities;
+            $parameters['capabilities']['alwaysMatch'] = $this->filter($this->remap(self::$legacyToW3c, $requiredCapabilities));
         }
 
         try {
@@ -59,28 +107,28 @@ class WebDriver extends AbstractWebDriver implements WebDriverInterface
                 'POST',
                 '/session',
                 $parameters,
-                array(CURLOPT_FOLLOWLOCATION => true)
+                [CURLOPT_FOLLOWLOCATION => true]
             );
         } catch (\Exception $e) {
             // fallback to legacy JSON Wire Protocol
-            $capabilities = $desiredCapabilities ?: array();
+            $capabilities = $desiredCapabilities ?: [];
             $capabilities[Capability::BROWSER_NAME] = $browserName;
 
-            $parameters = array('desiredCapabilities' => $capabilities);
+            $parameters = ['desiredCapabilities' => $this->remap(self::$w3cToLegacy, $capabilities)];
 
             if (is_array($requiredCapabilities) && count($requiredCapabilities)) {
-                $parameters['requiredCapabilities'] = $requiredCapabilities;
+                $parameters['requiredCapabilities'] = $this->remap(self::$w3cToLegacy, $requiredCapabilities);
             }
 
             $result = $this->curl(
                 'POST',
                 '/session',
                 $parameters,
-                array(CURLOPT_FOLLOWLOCATION => true)
+                [CURLOPT_FOLLOWLOCATION => true]
             );
         }
 
-        $this->capabilities = isset($result['value']['capabilities']) ? $result['value']['capabilities'] : null;
+        $this->capabilities = $result['value']['capabilities'] ?? null;
 
         $session = new Session($result['sessionUrl'], $this->capabilities);
 
@@ -98,7 +146,7 @@ class WebDriver extends AbstractWebDriver implements WebDriverInterface
     public function sessions()
     {
         $result = $this->curl('GET', '/sessions');
-        $sessions = array();
+        $sessions = [];
 
         foreach ($result['value'] as $session) {
             $session = new Session($this->url . '/session/' . $session['id'], $this->capabilities);
@@ -107,5 +155,40 @@ class WebDriver extends AbstractWebDriver implements WebDriverInterface
         }
 
         return $sessions;
+    }
+
+    /**
+     * Filter capabilities
+     *
+     * @param array $capabilities
+     *
+     * @return array
+     */
+    private function filter($capabilities)
+    {
+        return $capabilities ? array_values(array_filter($capabilities, fn($capability) => self::$w3cCapabilities[$capability] ?? 0)) : null;
+    }
+
+    /**
+     * Remap capabilities
+     *
+     * @param array $mapping
+     * @param array $capabilities
+     *
+     * @return array
+     */
+    private function remap($mapping, $capabilities)
+    {
+        $new = [];
+
+        foreach ($capabilities as $key => $value) {
+            if (array_key_exists($key, $mapping)) {
+                $key = $mapping[$key];
+            }
+
+            $new[$key] = $value;
+        }
+
+        return $new;
     }
 }
