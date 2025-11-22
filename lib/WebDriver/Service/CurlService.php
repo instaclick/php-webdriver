187672cde1c5befc1f97f67ebef77e3ae2ccb507
Christophe Coevoet <stof@notk.org>
2025-11-22 19:42:18 +0100

    Fix deprecations triggered by PHP 8.5
    The `curl_close` function is a no-op as of PHP 8.0 (when the curl
    extension migrated from resources to objects) and is deprecated as of
    PHP 8.5.

--- lib/WebDriver/Service/CurlService.php
+++ lib/WebDriver/Service/CurlService.php
@@ -89,7 +89,9 @@ class CurlService implements CurlServiceInterface
             CURLE_GOT_NOTHING !== ($errno = curl_errno($curl)) &&
             $error = curl_error($curl)
         ) {
-            curl_close($curl);
+            if (\PHP_VERSION_ID < 80000) {
+                curl_close($curl);
+            }
 
             $e = new CurlExecException(
                 sprintf(
@@ -106,8 +108,9 @@ class CurlService implements CurlServiceInterface
 
             throw $e;
         }
-
-        curl_close($curl);
+        if (\PHP_VERSION_ID < 80000) {
+            curl_close($curl);
+        }
 
         return array($rawResult, $info);
     }
