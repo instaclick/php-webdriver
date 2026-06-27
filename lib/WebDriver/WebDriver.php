b916e4fa6cba05ef9a200e5716e18a2a531cea00
Anthon Pang <apang@softwaredevelopment.ca>
2026-06-27 13:29:19 -0400

    Change str_contains() to strpos() for php 7.x compatibility

--- lib/WebDriver/WebDriver.php
+++ lib/WebDriver/WebDriver.php
@@ -166,7 +166,7 @@ class WebDriver extends AbstractWebDriver implements WebDriverInterface
      */
     private function filter($capabilities)
     {
-        return $capabilities ? array_filter($capabilities, function ($key) { return isset(self::$w3cCapabilities[$key]) || str_contains($key, ':'); }, ARRAY_FILTER_USE_KEY) : null;
+        return $capabilities ? array_filter($capabilities, function ($key) { return isset(self::$w3cCapabilities[$key]) || strpos($key, ':') !== false; }, ARRAY_FILTER_USE_KEY) : null;
     }
 
     /**
